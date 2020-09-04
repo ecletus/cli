4af3d053cf5f0bfe0be1176342021e298c5dc751
Moises P. Sena <moisespsena@gmail.com>
2020-09-04 09:55:19 -0300

    Add cli events

--- cli.go
+++ cli.go
@@ -7,28 +7,40 @@ import (
 
 	"github.com/ecletus/plug"
 	"github.com/moisespsena-go/default-logger"
+	"github.com/moisespsena-go/task"
 	"github.com/spf13/cobra"
+
+	"github.com/ecletus/ecletus"
 )
 
 var (
 	E_INIT     = PREFIX + ":init"
 	E_REGISTER = PREFIX + ":register"
-	log        = defaultlogger.NewLogger(PREFIX)
+	log        = defaultlogger.GetOrCreateLogger(PREFIX)
 )
 
 type Plugin struct {
-	plug.EventDispatcher
+	CliKey string
+	Cli *Cli
+}
+
+func (this Plugin) ProvideOptions() []string {
+	return []string{this.CliKey}
+}
+
+func (this *Plugin) Provides(options *plug.Options) {
+	options.Set(this.CliKey, this.Cli)
 }
 
 type Event struct {
 	plug.PluginEventInterface
-	CLI *CLI
+	CLI *Cli
 }
 
 type RegisterEvent struct {
 	plug.PluginEventInterface
 	RootCmd *cobra.Command
-	CLI     *CLI
+	CLI     *Cli
 }
 
 func OnRegisterE(dis plug.EventDispatcherInterface, cb func(e *RegisterEvent) error) error {
@@ -43,73 +55,111 @@ func OnRegister(dis plug.EventDispatcherInterface, cb func(e *RegisterEvent)) {
 	})
 }
 
-type CLI struct {
-	Dis                        plug.PluginEventDispatcherInterface
-	RootCmd                    cobra.Command
-	initCalled, registerCalled bool
+type CliOptions struct {
+	RootCmd *cobra.Command
+	Stderr io.Writer
+	Args []string
+}
+
+type Cli struct {
+	Ecletus                    *ecletus.Ecletus
+	RootCmd                    *cobra.Command
+	initCalled, 
+	registerCalled bool
 	args                       []string
 	cmdName                    string
 	doneCalled                 bool
-	doneCallbacks              []func()
+	InitCallbacks, 
+	DoneCallbacks,
+	RegisterCallbacks []func(cli *Cli) error
 	Stderr                     io.Writer
 }
 
-func (c *CLI) OnDone(cb ...func()) {
-	c.doneCallbacks = append(c.doneCallbacks, cb...)
+func New(ecl *ecletus.Ecletus, opt ...CliOptions) *Cli {
+	var opts CliOptions
+	for _, opts = range opt{}
+	if opts.RootCmd == nil {
+		opts.RootCmd = &cobra.Command{}
+	}
+	if opts.Args == nil {
+		opts.Args = os.Args
+	}
+	if opts.Stderr == nil {
+		opts.Stderr = os.Stderr
+	}
+	if opts.RootCmd.Use == "" {
+		opts.RootCmd.Use = opts.Args[0]
+		opts.Args = opts.Args[1:]
+	}
+	opts.RootCmd.SetArgs(opts.Args)
+
+	return &Cli{Ecletus: ecl, RootCmd: opts.RootCmd, Stderr: opts.Stderr, args: opts.Args}
 }
 
-func (c *CLI) Done() {
+func (c *Cli) OnDone(cb ...func(cli *Cli) error) {
+	c.DoneCallbacks = append(c.DoneCallbacks, cb...)
+}
+
+func (c *Cli) OnRegister(cb ...func(cli *Cli) error) {
+	c.RegisterCallbacks = append(c.RegisterCallbacks, cb...)
+}
+
+func (c *Cli) OnInit(cb ...func(cli *Cli) error) {
+	c.InitCallbacks = append(c.InitCallbacks, cb...)
+}
+
+func (c *Cli) Done() {
 	if c.doneCalled {
 		return
 	}
 	c.doneCalled = true
-	if c.doneCallbacks != nil {
+	if c.DoneCallbacks != nil {
 		log.Debug("calling done callbacks")
-		for i, cb := range c.doneCallbacks {
+		for i, cb := range c.DoneCallbacks {
 			func() {
 				defer func() {
 					if r := recover(); r != nil {
-						log.Error("done callback #%d failed: %v", i, r)
+						log.Error("done callback #%d panics: %v", i, r)
 					}
 				}()
-				cb()
+				if err := cb(c); err != nil {
+					log.Error("done callback #%d failed: %v", i, err)
+				}
 			}()
 		}
 	}
 }
 
-func (c *CLI) SetArgs(args []string) *CLI {
-	c.args = args
-	return c
+func (c *Cli) TriggerInit() (err error) {
+	for _, f := range c.InitCallbacks {
+		if err = f(c); err != nil {
+			return
+		}
+	}
+	return c.Ecletus.Container.Plugins.TriggerPlugins(&Event{plug.NewPluginEvent(E_INIT), c})
 }
 
-func (c *CLI) Args() (args []string) {
-	return c.args
-}
-
-func (c *CLI) SysArgs() *CLI {
-	c.RootCmd.Use = os.Args[0]
-	c.args = os.Args[1:]
-	return c
-}
-
-func (c *CLI) TriggerInit() error {
-	return c.Dis.TriggerPlugins(&Event{plug.NewPluginEvent(E_INIT), c})
-}
-
-func (c *CLI) Init() error {
+func (c *Cli) Init() (error) {
 	if !c.initCalled {
+		if err := c.Ecletus.Plugins().Add(&Plugin{}); err != nil {
+			return err
+		}
 		c.initCalled = true
 		return c.TriggerInit()
 	}
 	return nil
 }
 
-func (c *CLI) TriggerRegister() error {
-	return c.Dis.TriggerPlugins(&RegisterEvent{plug.NewPluginEvent(E_REGISTER), &c.RootCmd, c})
+func (c *Cli) TriggerRegister() (err error) {
+	for _, f := range c.RegisterCallbacks {
+		if err = f(c); err != nil {
+			return
+		}
+	}
+	return c.Ecletus.Container.Plugins.TriggerPlugins(&RegisterEvent{plug.NewPluginEvent(E_REGISTER), c.RootCmd, c})
 }
 
-func (c *CLI) Register() error {
+func (c *Cli) Register() (error) {
 	if !c.registerCalled {
 		c.registerCalled = true
 		return c.TriggerRegister()
@@ -117,11 +167,7 @@ func (c *CLI) Register() error {
 	return nil
 }
 
-func (c *CLI) Execute() (err error) {
-	if c.RootCmd.Use == "" {
-		c.SysArgs()
-	}
-
+func (c *Cli) Execute() (err error) {
 	defer c.Done()
 
 	if !c.initCalled {
@@ -130,9 +176,6 @@ func (c *CLI) Execute() (err error) {
 			return
 		}
 	}
-
-	c.RootCmd.SetArgs(c.args)
-
 	if !c.registerCalled {
 		err = c.Register()
 		if err != nil {
@@ -148,10 +191,25 @@ func (c *CLI) Execute() (err error) {
 	return 
 }
 
-func (c *CLI) ExecuteAlone() {
-	if err := c.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+type CliTask struct {
+	Cli *Cli
+}
+
+func NewTask(cli *Cli) *CliTask {
+	return &CliTask{Cli: cli}
+}
+
+func (this *CliTask) Setup(ta task.Appender) (err error) {
+	if err = this.Cli.Execute(); err != nil {
+		return 
 	}
-	os.Exit(0)
+	return this.Cli.Ecletus.Setup(ta)
+}
+
+func (this *CliTask) Start(done func()) (stop task.Stoper, err error) {
+	return this.Cli.Ecletus.Start(done)
+}
+
+func (this *CliTask) Run() (err error) {
+	return this.Cli.Ecletus.Run()
 }
